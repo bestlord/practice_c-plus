cda4363310aba5d5b96aa64c5f9dc4bf6044fc6b
bestlord <gameplay45@hotmail.com>
2021-01-10 02:16:46 +0700

    first test

---
+++ variable/inputvariable2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+    string support1, support2 ;
+    cout << "c++ suppport OS : " ;
+    getline (cin,support1) ;
+
+    cout << "c++ support OOP : " ;
+    getline (cin,support2) ;
+
+    return 0 ; 
+
+}
